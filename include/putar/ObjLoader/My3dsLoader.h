a865ae24ff3b55a3eb78b69ca20387d1131356b9
Garniewicz <matigar@op.pl>
2016-10-27 14:25:38 +0200

    add My3dsLoader.h & My3dsLoader.cpp

---
+++ include/putar/ObjLoader/My3dsLoader.h
@@ -0,0 +1,64 @@
+/** @file My3dsLoader.h
+ *
+ * implementation - My 3DS Loader
+ *
+ */
+
+#ifndef MY_3DS_LOADER_H_INCLUDED
+#define MY_3DS_LOADER_H_INCLUDED
+
+#include "objLoader.h"
+#include "../../3rdParty/tinyXML/tinyxml2.h"
+#include <iostream>
+#include <memory>
+
+namespace putar {
+    /// create a single ObjLoader
+    ObjLoader* createMy3dsLoader(void);
+    ObjLoader* createMy3dsLoader(std::string configFile);
+}
+
+using namespace putar;
+
+/// ObjLoader implementation
+class My3dsLoader : public ObjLoader {
+    public:
+
+        /// Pointer
+        typedef std::unique_ptr<My3dsLoader> Ptr;
+
+        /// Construction
+        My3dsLoader(void);
+
+        /// Construction
+        My3dsLoader(std::string configFilename) : ObjLoader("My3dsLoader", TYPE_3DS){
+            tinyxml2::XMLDocument config;
+            std::string filename = "../../resources/" + configFilename;
+            config.LoadFile(filename.c_str());
+            if (config.ErrorID())
+                std::cout << "unable to load ObjLoader config file.\n";
+            /*tinyxml2::XMLElement * posXML = config.FirstChildElement( "pose" );
+            double query[4];
+            posXML->QueryDoubleAttribute("qw", &query[0]); posXML->QueryDoubleAttribute("qx", &query[1]); posXML->QueryDoubleAttribute("qy", &query[2]); posXML->QueryDoubleAttribute("qz", &query[3]);
+            double queryPos[4];
+            posXML->QueryDoubleAttribute("x", &queryPos[0]); posXML->QueryDoubleAttribute("y", &queryPos[1]); posXML->QueryDoubleAttribute("z", &queryPos[2]);
+            pose = Quaternion (query[0], query[1], query[2], query[3])*Vec3(queryPos[0], queryPos[1], queryPos[2]);
+            */
+        }
+
+        /// Name of the grabber
+        const std::string& getName() const;
+
+        /// Returns the current 2D image
+        void loadObj(std::string filename);
+
+        /// Grab image and/or point cloud
+        void getMesh(Object3D& mesh) const;
+
+        void computeMask(cv::Mat& mask);
+
+    private:
+        Mat34 poseVis;
+};
+
+#endif // MY_3DS_LOADER_H_INCLUDED
---
+++ src/ObjLoader/My3dsLoader.cpp
@@ -0,0 +1,46 @@
+#include "ObjLoader/My3dsLoader.h"
+#include <memory>
+#include <stdexcept>
+#include <chrono>
+#include <thread>
+
+using namespace putar;
+
+/// A single instance of Kinect grabber
+My3dsLoader::Ptr my3dsLoader;
+
+MyLoader::My3dsLoader(void) : ObjLoader("My Loader", TYPE_3DS) {
+
+}
+
+const std::string& My3dsLoader::getName() const {
+    return name;
+}
+
+/// Returns the current 2D image
+void My3dsLoader::loadObj(std::string filename){
+    std::cout << filename << "\n";
+    throw std::runtime_error("LoadObj method is not implemented\n");
+}
+
+/// Grab image and/or point cloud
+void My3dsLoader::getMesh(Object3D& mesh) const{
+    std::cout << mesh.normals.size() << "\n";
+    throw std::runtime_error("getMesh method is not implemented\n");
+}
+
+void My3dsLoader::computeMask(cv::Mat& mask){
+    std::cout << mask.rows << "\n";
+    throw std::runtime_error("compute Mask method is not implemented\n");
+}
+
+putar::ObjLoader* putar::createMy3dsLoader(void) {
+    my3dsLoader.reset(new My3dsLoader());
+    return my3dsLoader.get();
+}
+
+putar::ObjLoader* putar::createMy3dsLoader(std::string configFile) {
+    my3dsLoader.reset(new My3dsLoader(configFile));
+    return my3dsLoader.get();
+}
+
