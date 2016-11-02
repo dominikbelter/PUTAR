aaa8ccdae543725a56a0b15a5bce51cb0bd556f5
wisnia2682 <bw2682@gmail.com>
2016-11-02 19:51:39 +0100

    Update imageVisualizer.h
    Edited ImageVisualizer.h

--- include/putar/ImageVisualizer/imageVisualizer.h
+++ include/putar/ImageVisualizer/imageVisualizer.h
@@ -5,15 +5,21 @@
 #include "Utilities/observer.h"
 #include <iostream>
 #include <string>
-#include <vector>
-#include <mutex>
 
-namaspace putar{
+namespace putar{
 	class ImageVisualizer{
 		public:
-					
+		
+			ImageVisualizer() {}
+			
+			// update the Cloud
 			virtual void updateCloud(cv::Mat RGBD) = 0;
+			
+			// update Mask
 			virtual void updateImage(cv::Mat Mask) = 0; 
+			
+			// Virtual destructor
+			virtual ~ImageVisualizer(){}
 	};
 }
 
